4bd37ac413af96f24bbbeddc9659f9b4f82b2143
Kazuki Tsujimoto <kazuki@callcc.net>
2014-07-06 21:01:19 +0900

    Extend #assert instead of defining #power_assert
    Now #power_assert is obsoleted.

--- lib/test/unit/power_assert.rb
+++ lib/test/unit/power_assert.rb
@@ -7,24 +7,24 @@ require 'test/unit/power_assert/version'
 require 'test/unit'
 require 'power_assert'
 
-module Test::Unit
-  module PowerAssert
-    module Assertions
-      def power_assert(&blk)
-        ::PowerAssert.start(blk, assertion_method: __method__) do |pa|
-          prc = pa.message_proc
-          class << prc
-            alias to_s call
-          end
-          assert_block(prc) do
-            pa.yield
-          end
+module Test::Unit::Assertions
+  alias __assert_orig__ assert
+
+  def assert(boolean = nil, message = nil, &blk)
+    if blk
+      ::PowerAssert.start(blk, assertion_method: __callee__) do |pa|
+        prc = pa.message_proc
+        class << prc
+          alias to_s call
+        end
+        assert_block(prc) do
+          pa.yield
         end
       end
+    else
+      __assert_orig__(boolean, message)
     end
   end
 
-  class TestCase
-    include PowerAssert::Assertions
-  end
+  alias power_assert assert
 end
--- test/test_power_assert.rb
+++ test/test_power_assert.rb
@@ -3,12 +3,23 @@ require 'test/unit/power_assert'
 
 class TestPowerAssert < Test::Unit::TestCase
   def test_successful
+    assert do
+      true
+    end
+    assert(true)
     power_assert do
       true
     end
   end
 
-  def test_failed
+  def test_assert_failed
+    return unless ENV['DEMO']
+    assert do
+      "0".class == "3".to_i.times.map {|i| i + 1 }.class
+    end
+  end
+
+  def test_power_assert_failed
     return unless ENV['DEMO']
     power_assert do
       "0".class == "3".to_i.times.map {|i| i + 1 }.class
