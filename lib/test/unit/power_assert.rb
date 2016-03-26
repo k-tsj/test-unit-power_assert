0564f8ed036359cfc4e89065883d77384a18f41a
Kazuki Tsujimoto <kazuki@callcc.net>
2016-03-26 18:27:01 +0900

    Warn if a user requires this library with test-unit 3 or later

--- lib/test/unit/power_assert.rb
+++ lib/test/unit/power_assert.rb
@@ -7,6 +7,14 @@ require 'test/unit/power_assert/version'
 require 'test/unit'
 require 'power_assert'
 
+begin
+  if Gem.loaded_specs['test-unit'].version >= Gem::Version.new('3')
+    warn "test-unit-power_assert: warning: You don't need to require test-unit-power_assert."
+    warn "test-unit-power_assert: warning: test-unit 3 or later has built-in support for power_assert."
+  end
+rescue
+end
+
 module Test::Unit::Assertions
   alias __assert_orig__ assert
 
